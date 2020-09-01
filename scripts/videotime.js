1c39cf2ed0c94de93a3747cf87220df3b9113049
chingya1214 <41608749+chingya1214@users.noreply.github.com>
2020-09-01 16:37:47 +0800

    3
    3

---
+++ scripts/videotime.js
@@ -0,0 +1,32 @@
+var x = document.getElementById("myVideo-1");
+function setCurTime1_1(){
+    x.currentTime = 14;
+}
+function setCurTime1_2(){
+    x.currentTime = 55;
+}
+function setCurTime1_3(){
+    x.currentTime = 165;
+}
+
+var x2 = document.getElementById("myVideo-2");
+function setCurTime2_1(){
+    x2.currentTime = 18;
+}
+function setCurTime2_2(){
+    x2.currentTime = 52;
+}
+function setCurTime2_3(){
+    x2.currentTime = 105;
+}
+
+var x3 = document.getElementById("myVideo-3");
+function setCurTime3_1(){
+    x3.currentTime = 50;
+}
+function setCurTime3_2(){
+    x3.currentTime = 103;
+}
+function setCurTime3_3(){
+    x3.currentTime = 119;
+}
